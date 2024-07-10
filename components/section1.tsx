e0434816afb08122b77a50bcf18674cc5bb47c49
Alexsander Souza <alexsander@oncoding.com.br>
2024-07-10 12:57:27 -0300

    legacy

---
+++ components/section1.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Section1() {
+  return (
+    <div className="flex size-full items-center justify-center text-white">
+      Section1
+    </div>
+  );
+}
