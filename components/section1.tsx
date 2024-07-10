dab4a260be662cf5e57f378cb54834c0fc173034
alexsanderOncoding <alexsander@oncoding.com.br>
2024-07-10 13:21:17 -0300

    chore

--- components/section1.tsx
+++ components/section1.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Section1() {
   return (
     <div className="flex size-full items-center justify-center text-white">
-      Section1
+      Section
     </div>
   );
 }
